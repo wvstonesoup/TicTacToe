7665160c479e43ee4ab57eaaf7fad8ce417c9d97
Sharon Smith <Sharon Smith>
2016-10-18 18:23:54 -0400

    some of this is unnecessary but what i have is working

--- board_print.rb
+++ board_print.rb
@@ -1,8 +1,25 @@
-# board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def empty_ttt_board
 
-# puts " #{board[0]} | #{board[1]} | #{board[2]} "
-# puts "-----------"
-# puts " #{board[3]} | #{board[4]} | #{board[5]} "
-# puts "-----------"
-# puts " #{board[6]} | #{board[7]} | #{board[8]} "
+	puts "Welcome to tic tac toe! This board accepts either 'x' or 'o' in the position you choose (1 through 9)" 
+	puts "when it is your turn. I will let you start."
+
+	board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+	puts " #{board[0]} | #{board[1]} | #{board[2]} "
+	puts "-----------"
+	puts " #{board[3]} | #{board[4]} | #{board[5]} "
+	puts "-----------"
+	puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+empty_ttt_board
+
+def player1
+
+	puts "Please pick a character of X or O."
+	player1 = gets.chomp
+end
+
+	puts "Are you ready to place your first entry? Please pick a position for your first #{player1}."
+
+
 
---
+++ test_board.rb
@@ -0,0 +1,5 @@
+def user_character
+	if user_character == "x"
+	else user_character == "o"
+	end
+end
